66063d52ec5c18da877165ee9c2b8fb8a9384774
dbnp <dbnpv2@gmail.com>
2021-09-15 11:07:58 -0400

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+games {'gta5'}
+
+fx_version 'cerulean'
+
+description 'Simple dev tool for visualizing object placement on Peds'
+
+author 'Tugamars'
+
+version '1.0.0'
+
+client_script 'client.lua'
